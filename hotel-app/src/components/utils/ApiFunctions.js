936f83ecf0c34aad93d14ddc674fa340089f2c66
cgaraydev <cgaraydev@gmail.com>
2025-03-18 14:04:10 -0300

    Commit

--- hotel-app/src/components/utils/ApiFunctions.js
+++ hotel-app/src/components/utils/ApiFunctions.js
@@ -5,14 +5,6 @@ export const api = axios.create({
 })
 
 
-/* 
-  The addRoom function sends a POST request to the /rooms/add/new-room endpoint 
-  with the photo, roomType, and roomPrice data. The photo is sent as a FormData object, 
-  which is a built-in JavaScript object that allows you to send files in a POST request. 
-  The roomType and roomPrice are sent as regular form data.
-  The function then checks the response status code. If the status code is 201, it returns true, 
-  indicating that the room was successfully added. Otherwise, it returns false.
-*/
 export async function addRoom(photo, roomType, roomPrice) {
     const formData = new FormData()
     formData.append("photo", photo)
@@ -28,10 +20,6 @@ export async function addRoom(photo, roomType, roomPrice) {
 
 }
 
-/* 
-  The getRoomTypes function sends a GET request to the /rooms/room-types endpoint. 
-  If the request is successful, it returns the room types data. Otherwise, it throws an error.
-*/
 export async function getRoomTypes() {
     try {
         const response = await api.get("/rooms/room-types")
